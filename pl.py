95ef9c5610e81308aee099004fe5f9f0d7fe5d6d
danbishop14 <69700884+danbishop14@users.noreply.github.com>
2023-01-19 17:09:04 -0700

    Update pl.py

--- pl.py
+++ pl.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 @st.cache
 def get_az_xlsx() -> pd.DataFrame:
-    file = r'C:\Users\Dan\Desktop\Coding\Datasets\Redfin_Data\Redfin_Zip_Realtor11_AZ_2023-01.csv'
+    file = r'C:\Users\Dan\Desktop\Coding\Datasets\Redfin_Data\Redfin_Zip_Realtor_CA_2022-10.csv'
     return(pd.read_csv(file))
 df = get_az_xlsx()
 
