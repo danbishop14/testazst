e700e2c6f4a2a606758510ab141ee7e6a12a9248
danbishop14 <69700884+danbishop14@users.noreply.github.com>
2023-01-19 17:15:26 -0700

    Update pl.py

--- pl.py
+++ pl.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import streamlit as st
 
-@st.cache
-def get_az_xlsx() -> pd.DataFrame:
-    file = r'C:\Users\Dan\Desktop\Coding\Datasets\Redfin_Data\Redfin_Zip_Realtor_CA_2022-10.csv'
-    return(pd.read_csv(file))
-df = get_az_xlsx()
+# @st.cache
+# def get_az_xlsx() -> pd.DataFrame:
+#     file = 'Redfin_Zip_Realtor_CA_2022-10.csv'
+#     return(pd.read_csv(file))
+# df = get_az_xlsx()
 
-st.dataframe(df)
+uploaded_file = st.file_uploader('Choose a file')
+if uploaded_file is not None:
+#read csv
+    df1=pd.read_csv(uploaded_file)
