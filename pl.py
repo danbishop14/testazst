8ca2c1a35d40d1c91a77b1430d16007e87ab9cc9
danbishop14 <69700884+danbishop14@users.noreply.github.com>
2023-01-23 17:17:59 -0700

    Update pl.py

--- pl.py
+++ pl.py
@@ -3,14 +3,21 @@ import streamlit as st
 import polars as pl
 
 #Full Data Caching
-df_polars = pl.read_csv("https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/zip_code_market_tracker.tsv000.gz",has_header=True,sep = '\t',ignore_errors=True)
-df = df_polars.head()
+# df_polars = pl.read_csv("https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/zip_code_market_tracker.tsv000.gz",has_header=True,sep = '\t',ignore_errors=True)
+# df = df_polars.head()
+# st.write(df)
 # @st.cache
 # def get_data() -> pd.DataFrame:
 #     return pd.read_csv(url, compression='gzip', sep='\t')
 # df = get_data()
 
+
+df = pd.read_csv(r'C:\Users\Dan\Desktop\Coding\Datasets\Redfin_Data\Redfin_Zip_Realtor_CA_2022-10.csv')
+st.write(df)
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
 #read csv
     df1=pd.read_csv(uploaded_file)
+
+
+# st.dataframe(df.head())
